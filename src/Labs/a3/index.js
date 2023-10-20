5fc448dc0fe71e0363010a57af0d9ca2e4c230cd
Ori98 <neonpabs@gmail.com>
2023-10-19 20:09:26 -0400

    Made the Todo list visible

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -3,11 +3,13 @@ import PathParameters from "./PathParameters";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./todo/TodoList";
 
 function Assignment3() {
  return (
    <div>
      <h1>Assignment 3</h1>
+     <TodoList/>
      <ConditionalOutput/>
      <Styles/>
      <Classes/>
