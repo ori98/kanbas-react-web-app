56dcfc176e6554bcc4e52623c0ca50bea5d3c5c9
Ori98 <neonpabs@gmail.com>
2023-12-01 16:17:26 -0500

    Removed the Home and the Search Tabs

--- src/project/nav.js
+++ src/project/nav.js
@@ -5,24 +5,11 @@ function Nav() {
   return (
     <div className="list-group">
       <Link
-        to="/project/home"
-        className={`list-group-item ${pathname.includes("home") && `active`} list-group-item-action`}
-      >
-        Home
-      </Link>
-      <Link
         to={`/project/admin/users`}
         className={`list-group-item  list-group-item-action ${pathname.includes("users") && `active`}`}
         >
         User Table
         </Link>
-      <Link
-        to="/project/search"
-        className={`list-group-item ${pathname.includes("search") && `active`} list-group-item-action`}
-      >
-        Search
-      </Link>
-
       <Link
         to="/project/signin"
         className={`list-group-item ${pathname.includes("signin") && `active`} list-group-item-action`}
